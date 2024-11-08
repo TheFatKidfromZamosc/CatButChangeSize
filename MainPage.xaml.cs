6c75e61a028ae42abab8a553fa5482c7acab2fd2
TheFatKidfromZamosc <witiuk61@gmail.com>
2024-11-08 13:36:38 +0100

    Harder, Better, Faster, Stronger

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace CatSlider
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+           
+        }
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            double value = args.NewValue;
+            Cat.HeightRequest = value;
+            displayLabel.Text = String.Format("The Slider value is {0}", value);
+        }
+    }
+
+}
